54ae8cf3862b8e06c3a4f5405d9da518b7acabc5
wen gong <wen.g.gong@gmail.com>
2019-09-22 19:08:46 -0400

    Databrick spark

---
+++ lesson-17-pyspark/spark-guide/databricks.README.md
@@ -0,0 +1,18 @@
+
+[SQL Guide](https://docs.databricks.com/spark/latest/spark-sql/index.html)
+
+[Introduction to DataFrames - Python](https://docs.databricks.com/spark/latest/dataframes-datasets/introduction-to-dataframes-python.html)
+
+[Data Sources](https://docs.databricks.com/spark/latest/data-sources/index.html)
+
+[Structured Streaming](https://docs.databricks.com/spark/latest/structured-streaming/index.html)
+
+[Machine Learning](https://docs.databricks.com/spark/latest/mllib/index.html)
+
+[Deep Learning](https://docs.databricks.com/applications/deep-learning/index.html)
+
+
+[Graph Analysis](https://docs.databricks.com/spark/latest/graph-analysis/index.html)
+
+
+[REST API 2.0](https://docs.databricks.com/api/latest/index.html)
---
+++ lesson-17-pyspark/spark-guide/sql/SparkSQL.sql
@@ -0,0 +1,121 @@
+-- Databricks notebook source
+--https://community.cloud.databricks.com
+
+show databases
+
+-- COMMAND ----------
+
+create database test_db
+
+-- COMMAND ----------
+
+show databases
+
+-- COMMAND ----------
+
+use test_db
+
+-- COMMAND ----------
+
+show tables
+
+-- COMMAND ----------
+
+use default
+
+-- COMMAND ----------
+
+show tables
+
+-- COMMAND ----------
+
+use test_db;
+select * from default.flights_parq
+
+-- COMMAND ----------
+
+SELECT current_database()
+
+-- COMMAND ----------
+
+create table flights_parquet as select * from default.flights_parq
+
+-- COMMAND ----------
+
+select count(*) from flights_parquet
+
+-- COMMAND ----------
+
+select * from flights_parquet where upper(DEST_COUNTRY_NAME) in ('UNITED STATES','EGYPT')
+
+-- COMMAND ----------
+
+SELECT
+  DEST_COUNTRY_NAME,
+  CASE WHEN upper(DEST_COUNTRY_NAME) = 'UNITED STATES' THEN 1
+       WHEN upper(DEST_COUNTRY_NAME) = 'EGYPT' THEN 0
+       ELSE -1 END as US_FLAG
+FROM flights_parquet
+
+-- COMMAND ----------
+
+CREATE VIEW IF NOT EXISTS flights_nest AS
+  SELECT (DEST_COUNTRY_NAME, ORIGIN_COUNTRY_NAME) as country, count FROM flights_parquet
+
+-- COMMAND ----------
+
+show tables
+
+-- COMMAND ----------
+
+select * from flights_nest
+
+-- COMMAND ----------
+
+SELECT country.*, count FROM flights_nest
+
+-- COMMAND ----------
+
+CREATE OR REPLACE TEMP VIEW flights_agg AS
+  SELECT DEST_COUNTRY_NAME, collect_list(count) as collected_counts
+  FROM default.flights_parq GROUP BY DEST_COUNTRY_NAME
+
+-- COMMAND ----------
+
+select * from flights_agg
+
+-- COMMAND ----------
+
+SELECT DEST_COUNTRY_NAME, explode(collected_counts)  FROM flights_agg where upper(DEST_COUNTRY_NAME) = 'UNITED STATES'
+
+-- COMMAND ----------
+
+show functions
+
+-- COMMAND ----------
+
+SHOW SYSTEM FUNCTIONS
+
+-- COMMAND ----------
+
+SHOW user FUNCTIONS
+
+-- COMMAND ----------
+
+SHOW FUNCTIONS "u*";
+
+-- COMMAND ----------
+
+SHOW FUNCTIONS LIKE "collect*";
+
+-- COMMAND ----------
+
+set spark.sql.shuffle.partitions=20
+
+-- COMMAND ----------
+
+set
+
+-- COMMAND ----------
+
+
