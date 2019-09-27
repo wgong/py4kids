cd728441038e52c6e8d698aee769f6ed1a5d6176
wen gong <wen.g.gong@gmail.com>
2019-09-27 07:25:02 -0400

    add sparksql mllib

---
+++ lesson-17-pyspark/spark-guide/python/Chapter_16_pyspark_app.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+
+if __name__ == '__main__':
+    from pyspark.sql import SparkSession
+    spark = SparkSession.builder \
+        .master("local") \
+        .appName("Word Count") \
+        .config("spark.some.config.option", "some-value") \
+        .getOrCreate()
+
+    print(spark.range(5000).where("id > 500").selectExpr("sum(id)").collect())
+
+
