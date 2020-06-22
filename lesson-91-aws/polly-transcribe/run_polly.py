e16d8158e111ab2dd623d1444af9fef6b95f2d45
wgong <wen.g.gong@gmail.com>
2020-06-21 22:47:27 -0400

    polly and transcribe

--- lesson-91-aws/polly-transcribe/run_polly.py
+++ lesson-91-aws/polly-transcribe/run_polly.py
@@ -19,9 +19,20 @@ def polly_speak(polly, input_file, format='mp3', voice='Joanna'):
             soundfile.write(soundBytes)
 
 if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print(f"python {__file__} file.txt [Voice]")
+        sys.exit(1)
     if len(sys.argv) > 1:
-        polly_speak(polly_client, sys.argv[1])
-        sys.exit(0)
-    else:
-        print(f"python {__file__} f1.txt")
-        sys.exit(1)   
+        input_file = sys.argv[1]
+
+    # https://docs.aws.amazon.com/polly/latest/dg/voicelist.html
+    # Female: Joanna, Kendra, Salli, Kimberly
+    # Female/Child: Ivy
+    # Male: Joey, Matthew
+    # Male/Child: Justin, Kevin
+    voice = 'Joanna'
+    if len(sys.argv) > 2:
+        voice = sys.argv[2]
+
+    polly_speak(polly_client, input_file, voice=voice)
+    sys.exit(0)
--- lesson-91-aws/polly-transcribe/run_transcribe.py
+++ lesson-91-aws/polly-transcribe/run_transcribe.py
@@ -1,27 +1,76 @@
 import os
 import sys
 import boto3
+import time
+import datetime
+import requests
 
 defaultRegion = 'us-east-1'
-defaultUrl = f'https://polly.{defaultRegion}.amazonaws.com'
+bucket_name = "wengong-lambda101"
 
-polly_client = boto3.client('transcribe', region_name=defaultRegion, endpoint_url=defaultUrl)
+s3 = boto3.resource('s3')
 
-def polly_speak(polly, input_file, format='mp3', voice='Joanna'):
-    with open(input_file) as f:
-        text = f.read()
-        resp = polly.synthesize_speech(OutputFormat=format, Text=text, VoiceId=voice)
-        soundBytes = resp['AudioStream'].read()
 
-        filename, _ = os.path.splitext(input_file)
-        output_file = f"{filename}-{voice}.{format}"
-        with open(output_file, 'wb') as soundfile:
-            soundfile.write(soundBytes)
+transcribe_client = boto3.client('transcribe')
+
+def transcribe(voice_file, bucket, format='mp3', lang='en-US'):
+    ts_start = datetime.datetime.now()
+    basename = os.path.basename(voice_file)
+    jobname, _ = os.path.splitext(basename)
+    
+    file_url = f"https://{bucket}.s3.amazonaws.com/{basename}"
+    print(f"{file_url}")
+
+    try:
+        response = transcribe_client.start_transcription_job(
+            TranscriptionJobName=jobname,
+            Media={'MediaFileUri': file_url},
+            MediaFormat=format,
+            LanguageCode=lang)
+        print(f"Request submitted: {response['ResponseMetadata']['RequestId']}")
+
+    except Exception as e:
+        print(e)
+        sys.exit()
+
+    for i in range(1,3600): # max wait = 1 hr
+        status = transcribe_client.get_transcription_job(TranscriptionJobName=jobname)
+        if status['TranscriptionJob']['TranscriptionJobStatus'] in ['COMPLETED', 'FAILED']:
+            ts_end = datetime.datetime.now()
+            break
+        print(f"{i}")
+        time.sleep(1)
+
+    print(f"processing time is {ts_end - ts_start}")
+
+    url_transcript = status['TranscriptionJob']['Transcript']['TranscriptFileUri']
+    print(f"transcript URL is {url_transcript}")
+
+    # extract transcript
+    response = requests.get(url_transcript)
+    response.raise_for_status()
+    # access JSOn content
+    jsonResponse = response.json()
+    transcript = " ".join([x['transcript'] for x in jsonResponse['results']['transcripts']])
+
+    tx_file = f"{jobname}-tx.txt"
+    with open(tx_file, "w") as f:
+        f.write(transcript)
+    return tx_file
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        polly_speak(polly_client, sys.argv[1])
-        sys.exit(0)
-    else:
-        print(f"python {__file__} f1.txt")
-        sys.exit(1)   
+    if len(sys.argv) < 2:
+        print(f"python {__file__} file.mp3")
+        sys.exit(1)
+
+    voice_file = sys.argv[1]
+
+
+    # upload voice file to S3
+    s3.Bucket(bucket_name).upload_file(voice_file, voice_file)
+
+    # run transcribe job
+    tx_file = transcribe(voice_file, bucket_name)
+    print(f"transcript saved in {tx_file}")
+
+    sys.exit(0)
