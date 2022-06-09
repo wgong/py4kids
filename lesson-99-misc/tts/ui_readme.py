d048c9ada6466c0c17504f8fd5a6d9fa5ce5c3ea
wwwgong <wen.g.gong@gmail.com>
2022-06-08 21:30:43 -0400

    url/pdf/docx to txt

--- lesson-99-misc/tts/ui_readme.py
+++ lesson-99-misc/tts/ui_readme.py
@@ -21,6 +21,9 @@ from streamlit_option_menu import option_menu
 from io import StringIO
 # import pandas as pd
 # import platform
+# import socket
+import urllib
+from bs4 import BeautifulSoup
 
 # Vanguard pkg
 # from identity import Identity as id_
@@ -33,18 +36,32 @@ st.set_page_config(
      initial_sidebar_state="expanded",
 )
 
+def _rename_voice(nm):
+    return ' - '.join(reversed([i.strip() for i in nm.split('-')]))
 
 # _CURRENT_USER = id_.username.casefold()
-
+SAMPLE_TEXT = """
+Thank you for using text-to-speech tool.
+感谢您使用文字转语音工具。
+Vielen Dank, dass Sie das Text-zu-Sprache-Tool verwenden.
+"""
 # cached functions
 # @st.experimental_singleton
 def get_engine():
     engine = pyttsx3.init()
     voice_map = dict()
+    # one-time fix for my laptop where Chinese (Taiwan) is not removed, it actually points to German
+    # hostname = socket.gethostname()
     for voice in engine.getProperty('voices'):
         voice_nm = voice.name.replace("Microsoft ", "").replace("Desktop ", "")
-        voice_map[voice_nm] = voice.id
+        voice_map[_rename_voice(voice_nm)] = voice.id
     return engine, voice_map
+
+def _fetch_text_from_url(url):
+    request = urllib.request.Request(url)
+    content = urllib.request.urlopen(request)
+    soup = BeautifulSoup(content)
+    return soup.body.get_text()    
 
 def _parse_uids(uids):
     return sorted(list(set([r.strip().upper() for r in uids.replace(","," ").replace(";"," ").split() if r.strip()])))
@@ -67,21 +84,35 @@ def do_tts():
 
     col_left,col_right = st.columns([3,1])
 
-    readme_text = "I will speak this text"
+    readme_text = SAMPLE_TEXT
     with col_right:
         uploaded_file = st.file_uploader("Upload a text file (.txt, .doc, .pdf)")
         if uploaded_file is not None:
             readme_text = StringIO(uploaded_file.getvalue().decode("utf-8")).read()
+
+            # TODO: add logic to extract text from .docx and .pdf
+            # http://automatetheboringstuff.com/chapter13/
+            # pdf2txt - https://stackoverflow.com/questions/55767511/how-to-extract-text-from-pdf-in-python-3-7
+            # 
             
     with col_left:
-        with st.form(key='apply_form'):
-            webpage_url = st.text_input("Webpage URL:", value="", key="webpage_url")
+
+        # with st.form(key='fetch_text'):
+        #     webpage_url = st.text_input("Webpage URL:", value="", key="webpage_url")
+        #     if st.form_submit_button("Fetch Text"):
+        #         url_text = _fetch_text_from_url(webpage_url)
+
+        with st.form(key='read_me'):
             readme_text = st.text_area('Text to Read (*):', value=readme_text, height=200, key="readme_text")
-            if st.form_submit_button("ReadMe"):
+            if st.form_submit_button("ReadMe"): 
                 for txt in [l.strip() for l in readme_text.split('\n') if l.strip()]:
                     engine.setProperty('voice', voice_map[voice_name])
                     engine.say(txt)
                 engine.runAndWait()
+
+        if st.button("Stop"):
+            if engine.isBusy():
+                engine.stop()
 
 menu_dict = {
     "Welcome": {"fn": do_welcome, "icon": "caret-right-square"},
@@ -109,7 +140,7 @@ def do_sidebar():
 
         if menu_item == "Text-to-Speech":
             _, voice_map = get_engine()
-            voice_name = st.radio("Which voice?", voice_map.keys(), index=0, key="voice_name")
+            voice_name = st.radio("Which voice?", sorted(list(voice_map.keys())), index=1, key="voice_name")
 
 ## Body
 def do_body():
