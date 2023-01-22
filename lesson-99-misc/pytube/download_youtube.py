6e0ae311d252276927a7f4eeed2ad5baf55e5f11
wwwgong <wen.g.gong@gmail.com>
2023-01-22 12:03:52 -0500

    fix bug in pytube script

--- lesson-99-misc/pytube/download_youtube.py
+++ lesson-99-misc/pytube/download_youtube.py
@@ -5,20 +5,42 @@ from pytube import YouTube
 from pathlib import Path
 from time import time
 
-# read json_data
-file_json = "youtube-data.json"
-file_yaml = "youtube-data.yaml"
+FILE_JSON = "youtube-data.json"
+FILE_YAML = "youtube-list.yaml"
+FILE_LOG = "youtube.log"
 
-if Path(file_json).exists():
-    with open(file_json) as f:
+def _get_url_dict(d, key="todo"):
+    res = d.get(key, [])
+    return res if res else []
+
+def _log(msg, file="pytube.log", print_flag=True, encoding="utf-8"):
+    msg = f"{msg}\n"
+    with open(file,"a+", encoding=encoding) as f:
+        f.write(msg)
+    if print_flag:
+        print(msg)
+
+
+# read yaml data
+with open(FILE_YAML) as f:
+    url_dict = yaml.load(f, Loader=yaml.FullLoader)
+    
+list_todo = _get_url_dict(url_dict, key="todo")
+if not list_todo:
+    import sys
+    sys.exit()
+
+list_done = _get_url_dict(url_dict, key="done")
+list_error = _get_url_dict(url_dict, key="error")
+
+# read json_data
+if Path(FILE_JSON).exists():
+    with open(FILE_JSON) as f:
         json_data = json.loads(f.read())
 else:
     json_data = {}
-
-with open(file_yaml) as f:
-    url_dict = yaml.load(f, Loader=yaml.FullLoader)
-
-for url in url_dict["todo"]:
+    
+for url in list_todo:
     yt = YouTube(url)
     try:
         json_data.update({url : yt.title})
@@ -28,26 +50,30 @@ for url in url_dict["todo"]:
     try:
         file_mp4 = f"{yt.title}.mp4"
         if Path(file_mp4).exists():
-            print(f"{file_mp4} already exists, skip...")
+            _log(f"{file_mp4} already exists, skip...")
         else:
-            print(f"Downloading {file_mp4} ...")
+            _log(f"Downloading {file_mp4} ...")
             t1 = time()
             subprocess.run(["pytube", url])
             t2 = time()
-            print(f"... completed in {(t2-t1):.2f} sec")
+            _log(f"... completed in {(t2-t1):.2f} sec")
 
-        if url not in url_dict["done"]:
-            url_dict["done"].append(url)
+        if url not in list_done:
+            list_done.append(url)
     except Exception as e:
-        print(f"[Error] str(e)")
-        if url not in url_dict["error"]:
-            url_dict["error"].append(url)
-
-# write yaml
-with open(file_yaml, "w") as f:
-    url_dict["todo"] = None
-    yaml.dump(url_dict, f)
+        _log(f"[Error] {str(e)}")
+        if url not in list_error:
+            list_error.append(url)
 
 # write json_data
-with open(file_json, "w") as f:
+with open(FILE_JSON, "w") as f:
     f.write(json.dumps(json_data))
+    
+# write yaml
+with open(FILE_YAML, "w") as f:
+    url_dict.update({
+            "todo": None,
+            "done": list_done if list_done else None,
+            "error": list_error if list_error else None
+        })
+    yaml.dump(url_dict, f)
--- lesson-99-misc/pytube/readme.md
+++ lesson-99-misc/pytube/readme.md
@@ -1,9 +1,19 @@
 # Install
-
-`pip install pytube`
+```
+pip install pytube
+```
 
 # Docs
 https://pytube.io/en/latest/user/quickstart.html
 
 # CLI
-`pytube https://www.youtube.com/watch?v=Xh5rFHYZS8Y`
+
+## run `pytube` exe on command line:
+```
+pytube https://www.youtube.com/watch?v=Xh5rFHYZS8Y
+```
+
+## enter video URL in `youtube-list.yaml` file,
+```
+python download_youtube.py
+```
