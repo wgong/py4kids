ed474b6a604dbc7b36d780913241d2b567def785
Gong, Wen <wen.g.gong@gmail.com>
2023-11-18 17:51:44 -0500

    moviepy

--- lesson-41-movie/readme.md
+++ lesson-41-movie/readme.md
@@ -7,12 +7,38 @@
 - https://github.com/parallel75/GPT_Multimodal/tree/main
 
 # Setup
+## install pytube (optional)
+download youtube videos (see https://github.com/wgong/py4kids/tree/master/lesson-99-misc/pytube)
 
+## install pyGame (optional) 
+
+## install imagemagick (required)
+
+https://www.imagemagick.org/script/download.php
+
+## clone source
 ```
 conda create -n movie
 conda activate movie
 
+cd ~\projects\wgong\FUN
+git clone git@github.com:Zulko/moviepy.git
+python setup.py install
+pip show moviepy
+
+# Name: moviepy
+# Version: 2.0.0.dev0
+# Summary: Video editing with Python
+# Home-page: https://zulko.github.io/moviepy/
+# Author: Zulko 2017
 ```
+not working because v2.0 is under development
+
+```
+pip install moviepy
+# Version: 1.0.3
+```
+
 
 ## local
 dir = ~\projects\wgong\FUN\moviepy
--- lesson-99-misc/pytube/download_youtube.py
+++ lesson-99-misc/pytube/download_youtube.py
@@ -1,3 +1,14 @@
+"""
+Instructions:
+1) add YouTube URL(s) to "youtube-list.yaml" under 1_todo
+2) $ python download_youtube.py
+3) downloaded media files are saved to sub-folder: "media"
+4) file "youtube-data.json" tracks downloaded files in the past
+
+
+pip install unicodedata2  # to normalize unicode
+"""
+
 import yaml
 import json
 import subprocess
@@ -6,14 +17,26 @@ from pathlib import Path
 from time import time
 from datetime import datetime
 import shutil
-
+import string
+# import re
+# import unicodedata
 
 
 FILE_JSON = "youtube-data.json"
 FILE_YAML = "youtube-list.yaml"
 FILE_LOG = "youtube.log"
+TARGET = "media"
 
-def _get_url_dict(d, key="todo"):
+TAG_TODO = "1_todo"
+TAG_ERROR = "2_error"
+TAG_DONE = "3_done"
+TAG_ARCHIVE = "4_archive"
+
+target_path = Path(TARGET)
+if not target_path.exists():
+    target_path.mkdir(parents=True, exist_ok=True)
+
+def _get_url_dict(d, key=TAG_TODO):
     res = d.get(key, [])
     return res if res else []
 
@@ -31,17 +54,31 @@ def _parse_title(url):
     return title
 
 
+
+def remove_punctuation(text):
+    """Removes punctuation from a Unicode string.
+
+    Args:
+        text: The Unicode string to remove punctuation from.
+
+    Returns:
+        A Unicode string with the punctuation removed.
+    """
+    table = str.maketrans('', '', string.punctuation.replace('.',''))
+    return text.translate(table).strip()
+
 # read yaml data
 with open(FILE_YAML) as f:
     url_dict = yaml.load(f, Loader=yaml.FullLoader)
     
-list_todo = _get_url_dict(url_dict, key="todo")
+list_todo = _get_url_dict(url_dict, key=TAG_TODO)
 if not list_todo:
     import sys
     sys.exit()
 
-list_done = _get_url_dict(url_dict, key="done")
-list_error = _get_url_dict(url_dict, key="error")
+list_done = _get_url_dict(url_dict, key=TAG_DONE)
+list_error = _get_url_dict(url_dict, key=TAG_ERROR)
+list_archive = _get_url_dict(url_dict, key=TAG_ARCHIVE)
 
 # read json_data
 if Path(FILE_JSON).exists():
@@ -51,6 +88,12 @@ else:
     json_data = {}
     
 for url in list_todo:
+    # skip if url found in youtube-data.json
+
+    if url in json_data:
+        print(f"Skip: {url} already downloaded, title: '{json_data[url]}' ")
+        continue
+
     # https://stackoverflow.com/questions/76129007/pytube-keyerror-streamdata-while-downloading-a-video
     # yt = YouTube(url, use_oauth=True, allow_oauth_cache=True)
     # fixed after upgrading pytube from 12.1.2 to 15.0.0
@@ -61,23 +104,31 @@ for url in list_todo:
         yt_title = _parse_title(url)
     json_data.update({url : str(yt_title)})
         
-    try:
-        file_mp4 = f"{yt_title}.mp4"
-        if Path(file_mp4).exists():
-            _log(f"{file_mp4} already exists, skip ...")
-        else:
-            _log(f"Downloading {file_mp4} \n\t from url = {url} ...")
-            t1 = time()
-            subprocess.run(["pytube", url])
-            t2 = time()
-            _log(f"...... completed in {(t2-t1):.2f} sec")
+    file_mp4 = remove_punctuation(yt_title) + ".mp4"
+    file_path = Path(file_mp4)
+    if file_path.exists():
+        _log(f"'{file_mp4}' already exists, skip ...")
+    else:
+        _log(f"Downloading '{file_mp4}' \n\t from url = {url} ...")
+        t1 = time()
+        p = subprocess.run(["pytube", url], stderr=subprocess.PIPE)
+        t2 = time()
+        if p.stderr:
+            err = p.stderr.decode("utf-8")
+            _log(f"[Error] {err}")
+            if url not in list_error:
+                err_msg = f"{url} == {err}"
+                list_error.append(err_msg)
+            continue
 
-        if url not in list_done:
-            list_done.append(url)
-    except Exception as e:
-        _log(f"[Error] {str(e)}")
-        if url not in list_error:
-            list_error.append(url)
+        if file_path.exists():
+            shutil.move(file_path, target_path/file_path)
+
+            if url not in list_done:
+                list_done.append(url)
+            _log(f"...... completed in {(t2-t1):.2f} sec")
+        else:
+            _log(f"[ERROR] failed to download {url}")
 
 # write json_data
 # https://stackoverflow.com/questions/18337407/saving-utf-8-texts-with-json-dumps-as-utf-8-not-as-a-u-escape-sequence
@@ -87,7 +138,7 @@ with open(FILE_JSON, "w", encoding="utf-8") as f:
     
 # backup FILE_YAML 
 ts = datetime.now().strftime("%Y-%m-%d_%H%M%S")
-shutil.copyfile(FILE_YAML, f"{FILE_YAML}.{ts}")
+shutil.copyfile(FILE_YAML, Path("log") / f"{FILE_YAML}.{ts}")
 ## 2nd option
 # shutil.copy(src, dst)   # dst can be a folder
 
@@ -95,8 +146,9 @@ shutil.copyfile(FILE_YAML, f"{FILE_YAML}.{ts}")
 # write yaml
 with open(FILE_YAML, "w") as f:
     url_dict.update({
-            "todo": None,
-            "done": list_done if list_done else None,
-            "error": list_error if list_error else None
+            TAG_TODO: None,
+            TAG_ERROR: list_error if list_error else None,
+            TAG_DONE: list_done if list_done else None,
+            TAG_ARCHIVE: list_archive if list_archive else None,
         })
     yaml.dump(url_dict, f)
