209c48375dc5200e6f970da6912ad1f65e09b24a
wen gong <wen.g.gong@gmail.com>
2024-06-14 16:53:23 -0400

    run vanna demo for 5 other LLM models

---
+++ lesson-18-ai/ollama-pull.sh
@@ -0,0 +1,11 @@
+# ollama pull llama2
+ollama pull codellama
+ollama pull mistral
+ollama pull neural-chat
+ollama pull starling-lm
+ollama pull orca-mini
+ollama pull vicuna
+ollama pull llava
+ollama pull llama2:13b
+ollama pull llama2-uncensored
+# ollama pull llama2:70b
