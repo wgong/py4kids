0c0c7188a2c5d92cc26e42d45b2de2db705ae7ab
Gong, Wen <wen.g.gong@gmail.com>
2025-07-04 02:47:07 -0400

    update

--- 001_run_git_sync.sh
+++ 001_run_git_sync.sh
@@ -1,3 +1,4 @@
 git pull
-cd lesson-99-misc/utils/git
-sh 00_run_git_sync.sh
+# cd lesson-99-misc/utils/git
+# sh 00_run_git_sync.sh -c 
+python lesson-99-misc/utils/git/git_sync.py -c git_sync_config.yaml
